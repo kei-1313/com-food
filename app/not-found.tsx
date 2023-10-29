5991bfb9e98415b0f302b605d8c3fe42e301e107
kei-1313 <candydog666@gmail.com>
2023-10-29 19:05:31 +0900

    エラーページと404ページを作成

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+// データが存在しないときの画面
+const NotFound = () => {
+  return (
+    <div className="pt-10">
+      <div className="text-center text-5xl font-bold mb-3">404</div>
+      <div className="text-center text-xl font-bold">Not Found</div>
+    </div>
+  )
+}
+
+export default NotFound
