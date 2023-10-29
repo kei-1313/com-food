743bd12aeabdb09e12d0f92400d95a3fd53b20cf
kei-1313 <candydog666@gmail.com>
2023-10-29 20:59:55 +0900

    supabase連携設定

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: ['espzlvhbcyudusyisxhv.supabase.co'],
+  },
+}
 
 module.exports = nextConfig
