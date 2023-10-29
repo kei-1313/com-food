1452ab01a6d07ed7f00d29c727ecc00902aa06bc
kei-1313 <candydog666@gmail.com>
2023-10-29 19:09:26 +0900

    layoutの設定

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="">
-      
-    </main>
+    <div className="">
+      <div>HOME</div>
+    </div>
   )
 }
