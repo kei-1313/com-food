244e83375009ea49c598298e3a8402156fa924ca
kei-1313 <candydog666@gmail.com>
2023-11-07 21:00:39 +0900

    ログイン状態を監視するためmiddleware.tsを作成

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+
+//NextRequestの型とDatabaseの型をimportしている
+import type { NextRequest } from 'next/server'
+import type  {Database } from '@/lib/database.types'
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient<Database>({ req, res })
+
+  //getSession関数により、sessionの情報を取得でき、ログインの監視ができる
+  await supabase.auth.getSession()
+  return res
+}
