a7741d325d1469279d6409a0387e1f5e32fbf482
kei-1313 <candydog666@gmail.com>
2023-11-14 21:18:17 +0900

    [add] ナビゲーションにuserの情報を追加

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -5,16 +5,18 @@ import Image from 'next/image'
 import { useEffect } from 'react'
 import type { Session } from '@supabase/auth-helpers-nextjs'
 import type { Database } from '@/lib/database.types'
-// type ProfileType = Database['public']['Tables']['profiles']['Row']
+type AdminUserType = Database['public']['Tables']['admin_users']['Row']
 
 
-const Navigation = ({session}: {session: Session | null}) => { 
+const Navigation = ({session, adminUser}: {session: Session | null, adminUser: AdminUserType | null}) => { 
 	return (
 		<header className="shadow-lg shadow-gray-100">
       <div className="py-5 px-5 container max-w-7xl mx-auto flex items-center justify-between">
         <Link href="/" className="font-bold text-xl cursor-pointer">
           com-food
         </Link>
+
+        <p>{adminUser?.email}</p>
       </div>
     </header>
 	)
