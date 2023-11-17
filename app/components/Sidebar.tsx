6669912546b2c067be22f9bc9258f52e97e7d536
kei-1313 <candydog666@gmail.com>
2023-11-17 23:55:25 +0900

    [add] sidebarContactコンポネントの追加ととsidebarコンポネントを編集

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -2,31 +2,31 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
-
-import {
-  Cog8ToothIcon,
-  ClipboardDocumentIcon
-} from '@heroicons/react/24/outline'
-
+import SidebarContact from './SidebarContact'
+import { useRouter, usePathname } from 'next/navigation'
+import SidebarProfile from './SidebarProfile'
 
 const Sidebar = () => {
+  const router = useRouter()
+  const currentUrl = usePathname()
+  console.log(currentUrl);
+  
+
 	return (
     <div className="flex">
       <div className="w-[60px] bg-blackPurple h-full min-h-screen flex flex-col justify-end">
-        <div className="w-[36px] mx-auto pb-4 cursor-pointer">
-          <Image src={'/icon_person.svg'} alt="avatar" width={14} height={14}/>
-        </div>
+        <Link href="/admin/user/" className="block">
+          <div className="w-[36px] mx-auto pb-4 cursor-pointer">
+            <Image src={'/icon_person.svg'} alt="avatar" width={14} height={14}/>
+          </div>
+        </Link>
       </div>
-      <div className="w-[200px] bg-lightPurple h-full min-h-screen px-4 py-2">
-        <ul>
-          <li className="px-2 py-2 rounded hover:bg-currentPurple border-gray-100 cursor-pointer">
-            <Link href="/setting/" className=" flex gap-2 items-center">
-              <ClipboardDocumentIcon width={22}/>
-              <span className="text-sm">お問い合わせ</span>
-            </Link>
-          </li>
-        </ul>
-      </div>
+      {currentUrl === '/admin/user' ? (
+        <SidebarProfile/>
+      ) :
+        <SidebarContact/>
+      }
+      
     </div>
 	)
 }
---
+++ app/components/SidebarContact.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import Link from 'next/link'
+import {
+  ClipboardDocumentIcon
+} from '@heroicons/react/24/outline'
+
+const SidebarContact = () => { 
+	return (
+		<div className="w-[200px] bg-lightPurple h-full min-h-screen px-4 py-2">
+        <ul>
+          <li className="px-2 py-2 rounded border-gray-100 cursor-pointer">
+            <Link href="/setting/" className=" flex gap-2 items-center">
+              <ClipboardDocumentIcon width={22}/>
+              <span className="text-sm">お問い合わせ</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+	)
+}
+
+export default SidebarContact
+
