49cff0bceaa8e3702809877ca01378b9e5f1e484
kei-1313 <candydog666@gmail.com>
2023-11-21 20:52:40 +0900

    [change]お問い合わせcssを調整

--- app/components/SidebarContact.tsx
+++ app/components/SidebarContact.tsx
@@ -7,16 +7,16 @@ import {
 
 const SidebarContact = () => { 
 	return (
-		<div className="w-[200px] bg-lightPurple h-full min-h-screen px-4 py-2">
-        <ul>
-          <li className="px-2 py-2 rounded border-gray-100 cursor-pointer">
-            <Link href="/setting/" className=" flex gap-2 items-center">
-              <ClipboardDocumentIcon width={22}/>
-              <span className="text-sm">お問い合わせ</span>
-            </Link>
-          </li>
-        </ul>
-      </div>
+		<div className="w-[200px] bg-lightPurple h-full min-h-screen px-4 py-2 ml-[60px]">
+      <ul>
+        <li className="px-2 py-2 rounded border-gray-100 cursor-pointer">
+          <Link href="/setting/" className=" flex gap-2 items-center">
+            <ClipboardDocumentIcon width={22}/>
+            <span className="text-sm">お問い合わせ</span>
+          </Link>
+        </li>
+      </ul>
+    </div>
 	)
 }
 
