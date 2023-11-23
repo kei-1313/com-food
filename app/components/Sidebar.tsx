d851d3caa5ee4f040b66b55f35171e45f7784449
kei-1313 <candydog666@gmail.com>
2023-11-23 15:33:01 +0900

    [add]サイドバーのアバターも画像を適用

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -5,19 +5,29 @@ import Image from 'next/image'
 import SidebarContact from './SidebarContact'
 import { useRouter, usePathname } from 'next/navigation'
 import SidebarProfile from './SidebarProfile'
+import useStore from '@/store'
+import { useEffect, useState } from 'react'
 
 const Sidebar = () => {
   const router = useRouter()
+  const { user } = useStore()
+  const [avatar, setAvatar] = useState('/icon_person.svg')
   const currentUrl = usePathname()
-  console.log(currentUrl);
-  
+
+  useEffect(() => {
+    if(user.avatar_url) {
+      setAvatar(user.avatar_url)
+    }
+  }, [user])
 
 	return (
     <div className="flex">
       <div className="w-[60px] bg-blackPurple h-full min-h-screen flex flex-col justify-end fixed top-0 left-0">
         <Link href="/admin/user/" className="block">
-          <div className="w-[36px] mx-auto pb-4 cursor-pointer">
-            <Image src={'/icon_person.svg'} alt="avatar" width={14} height={14}/>
+          <div className="w-[36px] mx-auto pb-4 cursor-pointer relative">
+            <div className="w-[36px] h-[36px] absolute top-[-36px] left-0 right-0 mx-auto">
+              <Image src={avatar} alt="avatar" fill/> 
+            </div>
           </div>
         </Link>
       </div>
