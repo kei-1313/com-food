d524c694d17197cdbeadb90a1626e4327479265e
kei-1313 <candydog666@gmail.com>
2023-11-23 18:33:22 +0900

    [add]パスワード変更機能作成

--- app/components/ChangePassword.tsx
+++ app/components/ChangePassword.tsx
@@ -3,29 +3,39 @@
 import Loading from '@/app/loading'
 import { useState } from 'react'
 
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+
 import { useForm, SubmitHandler } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as z from 'zod'
 import type { Database } from '@/lib/database.types'
+import { useRouter } from 'next/navigation'
 type Schema = z.infer<typeof schema>
 
 // 入力データの検証ルールを定義
 const schema = z.object({
-  currentPassword:z.string().min(6, { message: '6文字以上入力する必要があります。' }).regex(/^[a-zA-Z0-9]+$/, {message: "英大文字、英小文字、数字で入力してください"}),
   newPassword: z.string().min(6, { message: '6文字以上入力する必要があります。' }).regex(/^[a-zA-Z0-9]+$/, {message: "英大文字、英小文字、数字で入力してください"}),
   confirmNewPassword: z.string().min(6, { message: '6文字以上入力する必要があります。' }).regex(/^[a-zA-Z0-9]+$/, {message: "英大文字、英小文字、数字で入力してください"})
 })
+.refine((data) => data.newPassword === data.confirmNewPassword, {
+  message: '新しいパスワードと確認用パスワードが一致しません',
+  path: ['confirmNewPassword']
+})
 
-const ResetPassword = () => {
+const ChangePassword = () => {
+  const router = useRouter()
+  const supabase = createClientComponentClient<Database>()
   const [loading, setLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
+  const [successMessage, setSuccessMessage] = useState('')
 
   const {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm({
     defaultValues: {
-      currentPassword: '',
       newPassword: '',
       confirmNewPassword: ''
     },
@@ -33,43 +43,60 @@ const ResetPassword = () => {
   })
 
   const onSubmit: SubmitHandler<Schema> = async (data) => {
-    
+    setLoading(true)
+    setErrorMessage('')
+    try {
+      // パスワードの更新
+      const { error } = await supabase.auth.updateUser({
+        password: data.newPassword,
+      })
+
+      if (error) {
+        setErrorMessage('エラーが発生しました。' + error.message)
+        return
+      }
+
+      reset()
+      setSuccessMessage('パスワード変更が変更されました')
+    } catch (error) {
+      setErrorMessage('エラーが発生しました。' + error)
+      return
+    }finally {
+      setErrorMessage('')
+      router.refresh()
+    }
   }
 
 	return (
 		<div className="p-10 w-[560px]">
 			<h2 className="text-2xl font-bold mb-6">パスワード変更</h2>
+      <p className="text-sm text-red-500 mt-2 mb-4 font-bold">{ errorMessage }</p>
+      <p className="text-sm text-green-500 mt-2 mb-4 font-bold">{ successMessage }</p>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mt-8">
-            <label className="text-base font-bold mb-2 block">現在のパスワード</label>
-            <input 
-              type="text" 
-              className="text-sm rounded border-gray-200 border-[1px] block py-3 px-3 w-full focus:border-activePurple focus:border-[2px] focus:outline-none"
-              {...register('currentPassword', { required: true })}
-              required
-            />
-            <div className="my-3 text-sm text-red-500">エラーです</div>
-        </div>
         <div className="mt-8">
             <label className="text-base font-bold block mb-2">新しいパスワード</label>
             <span className="text-xs text-attentionPurple block mb-2">6文字以上で入力してください。大文字、小文字、数字を含める必要があります。</span>
             <input 
-              type="text"
+              type="password"
+              id="newPassword"
+              placeholder="新しいパスワード"
               className="text-sm rounded border-gray-200 border-[1px] block py-3 px-3 w-full focus:border-activePurple focus:border-[2px] focus:outline-none"
               {...register('newPassword', { required: true })}
-              required
+              autoComplete="on"
             />
-            <div className="my-3 text-sm text-red-500">エラーです</div>
+            <div className="my-3 text-sm text-red-500">{errors.newPassword?.message}</div>
         </div>
         <div className="mt-8">
             <label className="text-base font-bold mb-2 block">新しいパスワード（確認）</label>
             <input 
-              type="text" 
+              type="password"
+              id="confirmNewPassword"
+              placeholder="確認用パスワード"
               className="text-sm rounded border-gray-200 border-[1px] block py-3 px-3 w-full focus:border-activePurple focus:border-[2px] focus:outline-none"
               {...register('confirmNewPassword', { required: true })}
-              required
+              autoComplete="on"
             />
-            <div className="my-3 text-sm text-red-500">エラーです</div>
+            <div className="my-3 text-sm text-red-500">{errors.confirmNewPassword?.message}</div>
         </div>
         <div className="mt-8">
 				{loading ? (
@@ -83,5 +110,5 @@ const ResetPassword = () => {
 	)
 }
 
-export default ResetPassword
+export default ChangePassword
 
