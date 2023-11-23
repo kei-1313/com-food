4101fe0dfcc1ee8439da9d72d4b0b47f62f2b9c4
kei-1313 <candydog666@gmail.com>
2023-11-23 15:25:40 +0900

    [add]ユーザ情報をストアに格納

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -5,10 +5,24 @@ import Image from 'next/image'
 import { useEffect } from 'react'
 import type { Session } from '@supabase/auth-helpers-nextjs'
 import type { Database } from '@/lib/database.types'
+import useStore from '@/store'
 type AdminUserType = Database['public']['Tables']['admin_users']['Row']
 
 
 const Navigation = ({session, adminUser}: {session: Session | null, adminUser: AdminUserType | null}) => {
+  const { setUser } = useStore()
+
+  useEffect(() => {
+    setUser({
+      id: session ? session.user.id : '',
+      created_at: session && adminUser ? adminUser.created_at : '',
+      email: session ? session.user.email! : '',
+      name: session && adminUser ? adminUser.name : '',
+      avatar_url: session && adminUser ? adminUser.avatar_url: '',
+      authority_id: session && adminUser ? adminUser.authority_id : 0
+    })
+  },[session, setUser, adminUser])
+
 	return (
     <div>
       {session ?
