911696e1f98e12b54d82c0517b09304dfd5d44d7
kei-1313 <candydog666@gmail.com>
2023-11-25 18:10:59 +0900

    [add]お問い合わせの管理画面のUIを作成

---
+++ app/admin/contact/page.tsx
@@ -0,0 +1,26 @@
+import { cookies } from 'next/headers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { redirect } from 'next/navigation'
+
+import type { Database } from '@/lib/database.types'
+import AdminContact from '@/app/components/AdminContact'
+
+const AdminContactPage = async () => {
+  const supabase = createServerComponentClient<Database>({
+    cookies,
+  })
+
+  // セッションの取得
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  // 未認証の場合、リダイレクト
+  if (!session) {
+    redirect('/auth/login')
+  }
+
+  return  <AdminContact />
+}
+
+export default AdminContactPage
