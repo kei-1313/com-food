ba0c8b25dccf540d0827ece6e5d9ba7b14d8326b
kei-1313 <candydog666@gmail.com>
2023-11-25 12:31:54 +0900

    [add]パスワードリセット機能作成

---
+++ app/auth/password-reset/confirm/page.tsx
@@ -0,0 +1,12 @@
+import ResetPasswordConfirm from '@/app/components/ResetPasswordConfirm'
+
+// パスワード再設定ページ
+const ResetPasswordConfirmPage = () => {
+  return (
+    <div className="max-w-[400px] mx-auto">
+      <ResetPasswordConfirm />
+    </div>
+  )
+}
+
+export default ResetPasswordConfirmPage
