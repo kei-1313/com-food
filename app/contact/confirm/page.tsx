2d53447c36f73b9c1508b5b12f8eb6b8a61ea0e4
kei-1313 <candydog666@gmail.com>
2023-12-02 19:17:13 +0900

    [delete]confirmディレクトリ削除

--- app/contact/confirm/page.tsx
+++
@@ -1,7 +0,0 @@
-import UserContactConfirm from '@/app/components/UserContactConfirm'
-
-const UserContactConfirmPage = async () => {
-  return  <UserContactConfirm />
-}
-
-export default UserContactConfirmPage
