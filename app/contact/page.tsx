e2ba8ff163bc53599b127f8fd93badadfec5f3da
kei-1313 <candydog666@gmail.com>
2023-12-02 19:13:43 +0900

    [add]確認画面作成完了

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,57 @@
-import UserContact from '@/app/components/UserContact'
+"use client"
 
-const UserContactPage = async () => {
-  return  <UserContact />
+import UserContact from '@/app/components/UserContact'
+import { useState } from 'react';
+
+import { useForm, FormProvider } from "react-hook-form";
+import { useRouter, useSearchParams } from 'next/navigation'
+import UserContactConfirm from '../components/UserContactConfirm';
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+
+type Schema = z.infer<typeof schema>
+
+// 入力データの検証ルールを定義
+const schema = z.object({
+	name: z.string(),
+  email: z.string().email({ message: 'メールアドレスの形式ではありません。' }),
+	subject: z.string(),
+	content: z.string().min(1,  { message: '一文字以上で入力してください' }).max(400, { message: '400文字以内で入力してください' })
+})
+
+
+const UserContactPage = () => {
+  const searchParams = useSearchParams();
+  const isConfirm = searchParams.get("confirm")
+
+  // console.log(isConfirm);
+  
+  const methods = useForm<Schema>({
+    //初期値
+    defaultValues: {name: '', email: '', subject: '', content: ''},
+    //入力値の検証
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+
+  // console.log(methods);
+  
+
+  
+
+  return(
+    <div>
+      <FormProvider {...methods}>
+        {!isConfirm ? (
+          <UserContact />
+        ) : (
+          <UserContactConfirm />
+        )}
+
+      </FormProvider>
+
+    </div>
+  )
 }
 
 export default UserContactPage
