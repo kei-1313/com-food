1bb1fd19e2c071840bed57aff63cbcb752562da1
kei-1313 <candydog666@gmail.com>
2024-02-04 12:44:58 +0900

    URLの変更

--- app/components/SidebarContact.tsx
+++ app/components/SidebarContact.tsx
@@ -10,7 +10,7 @@ const SidebarContact = () => {
 		<div className="w-[200px] bg-lightPurple h-full min-h-screen px-4 py-2 ml-[60px]">
       <ul>
         <li className="px-2 py-2 rounded border-gray-100 cursor-pointer">
-          <Link href="/setting/" className=" flex gap-2 items-center">
+          <Link href="/admin/contact/" className=" flex gap-2 items-center">
             <ClipboardDocumentIcon width={22}/>
             <span className="text-sm">お問い合わせ</span>
           </Link>
