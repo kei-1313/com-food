ee8e851713164e013435043002e869a21ef1bcf6
kei-1313 <candydog666@gmail.com>
2024-02-04 13:25:44 +0900

    Homeの整理

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,11 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
 
-import type  {Database } from '@/lib/database.types'
-import Sidebar from './components/Sidebar'
 
 const Home = async () => {
-  const supabase = createServerComponentClient<Database>({
-    cookies
-  })
-
-  //セッションの取得
-  const { data: {session} } = await supabase.auth.getSession()
-
   return (
     <div>
-      {session ?
-      <div>
-        <Sidebar/>
-      </div> :
       <div>
         未ログイン
       </div>
-      }
     </div>
   )
 }
