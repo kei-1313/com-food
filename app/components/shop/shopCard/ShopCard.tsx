24caa1cfee4b07e1bf2e024f4a33274ff7372b25
kei-1313 <candydog666@gmail.com>
2024-02-06 23:54:28 +0900

    店舗情報を整理

--- app/components/shop/shopCard/ShopCard.tsx
+++ app/components/shop/shopCard/ShopCard.tsx
@@ -7,29 +7,18 @@ interface ShopProps {
 }
 
 const ShopCard:React.FC<ShopProps> = ({shop}) => {
-  // const [shopItem, setShopItem] = useState({})
-
-  // useEffect(() => {
-  //   setShopItem(shop)
-  // }, [])
-
-  // console.log(shopItem);
-
-  // console.log(shop);
   let shopImage
   if(shop?.photos !== undefined) {
     shopImage = shop.photos[0].getUrl({'maxWidth':300, 'maxHeight':300})
   } else {
     shopImage = null
   }
-
-  
-  
+  console.log(shop);
   
 	return (
     <>
       {shop && 
-        <div className="flex justify-between gap-3 mb-4 max-sm:block">
+        <div className="flex justify-between gap-3 mb-4 max-sm:block items-center">
           <div className="w-[30%] relative max-sm:w-full">
             {shopImage !== null ? (
               <div className="w-full">
@@ -39,28 +28,27 @@ const ShopCard:React.FC<ShopProps> = ({shop}) => {
               <Image src="/icon_person.svg" alt="avatar" width={200} height={200}/>
             )}
           </div>
-          <div className="w-[70%] bg-slate-500 px-5 py-4 max-sm:w-full">
+          <div className="w-[70%] px-5 py-4 max-sm:w-full">
             <div className="mb-5 flex justify-between">
-              <div>
+              <div className="flex gap-2 items-center">
+                <img className="w-[20px] h-[20px]" src={shop.icon} alt=""/>
                 <h3 className="text-lg font-bold">{shop.name}</h3>
               </div>
               <div>
-                <span>評価 : {shop.rating}</span>
+                <span className="bg-[#3EB36D] block px-4 py-3 font-bold">営業中</span>
               </div>
             </div>
-            <div className="mb-5"> 
+            <div className="mb-5">
+              <p className="font-bold mb-2">住所</p>
               <p>{shop.vicinity}</p>
             </div>
-            <div className="mb-5 flex gap-4">
+            <div className="mb-5 flex gap-7">
               <div>
-                <p>営業時間</p>
+                <p className="font-bold">評価 : <span className="font-normal">{shop.rating}</span></p>
               </div>
               <div>
-                <p>口コミ : {shop.user_ratings_total}件</p>
+                <p className="font-bold">口コミ : <span className="font-normal">{shop.user_ratings_total}件</span></p>
               </div>
-            </div>
-            <div>
-              <p>店舗詳細店舗詳細店舗詳細店舗詳細店舗詳細店舗詳細店舗詳細店舗詳細店舗詳細店舗詳細店舗詳細</p>
             </div>
           </div>
         </div>
--- app/components/shop/shopCardList/ShopCardList.tsx
+++ app/components/shop/shopCardList/ShopCardList.tsx
@@ -11,7 +11,7 @@ const ShopCardList:React.FC<ShopsProps> = ({shops}) => {
 		<div className="max-w-[1200px] mx-auto px-5">
       <h2 className="text-2xl font-bold mb-6 pl-5 max-sm:pl-0 max-sm:mb-4">店舗一覧</h2>
       <div className="flex gap-4 mb-5 max-sm:mb-4 max-sm:gap-3">
-        <button className="px-6 py-4 max-sm:px-3 max-sm:py-3 bg-orange">本日営業中の店舗</button>
+        <button className="px-6 py-4 max-sm:px-3 max-sm:py-3 bg-[#3EB36D] font-bold">本日営業中の店舗</button>
         <button className="px-6 py-4 max-sm:px-3 max-sm:py-3 bg-sky-500">本日定休日の店舗</button>
       </div>
       {shops.map((shop, index) => (
