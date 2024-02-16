2bc914f74de5549c70c9444c80451d126a92dd02
kei-1313 <candydog666@gmail.com>
2024-02-17 01:03:41 +0900

    komenntoautosakuzyo

--- app/api/shop/route.ts
+++ app/api/shop/route.ts
@@ -1,15 +1,8 @@
 import { NextResponse, NextRequest } from 'next/server'
 
 export async function GET(req:NextRequest){
-  // console.log(req);
   const { searchParams } = new URL(req.url)
   const query = searchParams.get('query')
-  // console.log(queryText);
-  // const lat = req.nextUrl.searchParams.get('lat')
-  // const lng = req.nextUrl.searchParams.get('lng')
-  // const query = "ラーメン"
-  // const lat = "35.72295079725532"
-  // const lng = "139.71215183258244"
   const location = "43.072372229883534%2C141.3494175181069"
   const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY
   const radius = 1000; // 検索範囲（メートル）
